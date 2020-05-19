521851c921605bd5a9287b6b9531dcc94435ae62
John Jung <john@fieldrotation.com>
2020-05-19 13:16:20 -0500

    CLI hack- input from stdin, output to stdout.

--- iiif-presentation-validator.py
+++ iiif-presentation-validator.py
@@ -2,24 +2,17 @@
 # encoding: utf-8
 """IIIF Presentation Validation Service."""
 
-import argparse
 import codecs
 import json
 import os
+import sys
 from gzip import GzipFile
 from io import BytesIO
 from jsonschema.exceptions import ValidationError, SchemaError
 
-try:
-    # python3
-    from urllib.request import urlopen, HTTPError, Request
-    from urllib.parse import urlparse
-except ImportError:
-    # fall back to python2
-    from urllib2 import urlopen, HTTPError, Request
-    from urlparse import urlparse
+from urllib.request import urlopen, HTTPError, Request
+from urllib.parse import urlparse
 
-from bottle import Bottle, request, response, run
 from schema import schemavalidator
 
 egg_cache = "/path/to/web/egg_cache"
@@ -98,119 +91,28 @@ class Validator(object):
                 'error': str(err),
                 'url': url
             }
-        return self.return_json(infojson)
-
-    def return_json(self, js):
-        """Set header and return JSON response."""
-        response.content_type = "application/json"
-        return json.dumps(js)
-
-    def do_POST_test(self):
-        """Implement POST request to test posted data at default version."""
-        data = request.json
-        if not data:
-            b = request._get_body_string()
-            try:
-                b = b.decode('utf-8')
-            except:
-                pass
-            data = json.loads(b)
-        return self.check_manifest(data, self.default_version)
-
-    def do_GET_test(self):
-        """Implement GET request to test url at version."""
-        url = request.query.get('url', '')
-        version = request.query.get('version', self.default_version)
-        url = url.strip()
-        parsed_url = urlparse(url)
-        if (parsed_url.scheme != 'http' and parsed_url.scheme != 'https'):
-            return self.return_json({'okay': 0, 'error': 'URLs must use HTTP or HTTPS', 'url': url})
-
-        try:
-            (data, webhandle) = self.fetch(url)
-        except Exception as error:
-            return self.return_json({'okay': 0, 'error': 'Cannot fetch url. Got "{}"'.format(error), 'url': url})
-
-        # First check HTTP level
-        ct = webhandle.headers.get('content-type', '')
-        cors = webhandle.headers.get('access-control-allow-origin', '')
-
-        warnings = []
-        if not ct.startswith('application/json') and not ct.startswith('application/ld+json'):
-            # not json
-            warnings.append("URL does not have correct content-type header: got \"%s\", expected JSON" % ct)
-        if cors != "*":
-            warnings.append("URL does not have correct access-control-allow-origin header:"
-                            " got \"%s\", expected *" % cors)
-
-        content_encoding = webhandle.headers.get('Content-Encoding', '')
-        if content_encoding != 'gzip':
-            warnings.append('The remote server did not use the requested gzip'
-                            ' transfer compression, which will slow access.'
-                            ' (Content-Encoding: %s)' % content_encoding)
-        elif 'Accept-Encoding' not in webhandle.headers.get('Vary', ''):
-            warnings.append('gzip transfer compression is enabled but the Vary'
-                            ' header does not include Accept-Encoding, which'
-                            ' can cause compatibility issues')
-
-        return self.check_manifest(data, version, url, warnings)
-
-    def index_route(self):
-        """Read and return index page."""
-        with codecs.open(os.path.join(os.path.dirname(__file__), 'index.html'), 'r', 'utf-8') as fh:
-            data = fh.read()
-        return data
-
-    def dispatch_views(self):
-        """Set up path mappings."""
-        self.app.route("/", "GET", self.index_route)
-        self.app.route("/validate", "OPTIONS", self.empty_response)
-        self.app.route("/validate", "GET", self.do_GET_test)
-        self.app.route("/validate", "POST", self.do_POST_test)
-
-    def after_request(self):
-        """Used with after_request hook to set response headers."""
-        methods = 'GET,POST,OPTIONS'
-        headers = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = methods
-        response.headers['Access-Control-Allow-Headers'] = headers
-        response.headers['Allow'] = methods
-
-    def empty_response(self, *args, **kwargs):
-        """Empty response."""
-
-    def get_bottle_app(self):
-        """Return bottle instance."""
-        self.app = Bottle()
-        self.dispatch_views()
-        self.app.hook('after_request')(self.after_request)
-        return self.app
-
-
-def apache():
-    """Run as WSGI application."""
-    v = Validator()
-    return v.get_bottle_app()
+        return json.dumps(infojson)
 
 
 def main():
-    """Parse argument and run server when run from command line."""
-    parser = argparse.ArgumentParser(description=__doc__.strip(),
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('--hostname', default='localhost',
-                        help='Hostname or IP address to bind to (use 0.0.0.0 for all)')
-    parser.add_argument('--port', default=8080, type=int,
-                        help='Server port to bind to. Values below 1024 require root privileges.')
-
-    args = parser.parse_args()
+    data = sys.stdin.read()
+    version = '2.1'
 
     v = Validator()
-    run(host=args.hostname, port=args.port, app=v.get_bottle_app())
 
+    js = json.loads(v.check_manifest(data, version))
+
+    sys.stdout.write('OKAY: {}\n\n'.format(js['okay']))
+
+    sys.stdout.write('WARNINGS:\n')
+    for w in js['warnings']:
+        sys.stdout.write(w)
+    sys.stdout.write('\n')
+
+    sys.stdout.write('ERROR:\n')
+    sys.stdout.write(js['error'])
+    sys.stdout.write('\n')
+    
 
 if __name__ == "__main__":
     main()
-else:
-    application = apache()
